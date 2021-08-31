61b82b75827435ad0af68a8b8578938db94c5786
Pc <carloseduardomorenocamacho@gmail.com>
2021-08-30 23:51:47 -0700

    Email and password validated

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,7 +1,32 @@
 const { response } = require("express");
+const Usuario = require("../models/usuario");
+const bcrypt = require('bcryptjs');
 
 const login = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
     try {
+        //Verifica si el usuario ingreso el email correcto
+
+        const usuarioDB = await Usuario.findOne({ email });
+        //!usarioDB verifica si existe el usuario si no existe entonces ... ver if
+        if (!usuarioDB) {
+            return res.status(404).json({
+                msg: 'Email no encontrado '
+            });
+        }
+        //Verificar password, Si hace match con la contrasena del usuario regresaria un true 
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: true,
+                msg: 'Contrasenia no valida o incorrectaa'
+            })
+        }
+        // Si llega hasta aqui significa que el usuario y la contrasena son correctas entonces hay que generar un JWT
+
+
 
         res.json({
             ok: true,
